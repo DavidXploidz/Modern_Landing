3ec1711cfd2979122019f597db9adf8e9a598bd3
DavidXploidz <david.adame.itd@gmail.com>
2024-09-10 13:41:16 -0600

    feat: gsap added for animated elements

---
+++ app.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    gsap.registerPlugin(ScrollTrigger)
+    // gsap code here!
+    const main_tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: '.main',
+            start: 'top 80%',
+            end: 'top 20%',
+            scrub: 1,
+        }
+    });
+    main_tl.from(".main__title", {x: -500, opacity: 0, filter: "blur(10px)"})
+    main_tl.from(".main__text", {x: 500, opacity: 0, filter: "blur(10px)"})
+
+    const main_card = gsap.timeline({
+        scrollTrigger: {
+            trigger: '.main__grid',
+            start: 'top 80%',
+            end: 'top 20%',
+            scrub: 1,
+        }
+    });
+
+    const cardsMain = document.querySelectorAll('.card');
+
+    for(let i = 0; i < cardsMain.length; i++){
+        const card = cardsMain[i];
+        if(i % 2){
+            main_card.from(card, {x: -500, opacity: 0, filter: "blur(10px)"})
+        }else{
+            main_card.from(card, {x: 500, opacity: 0, filter: "blur(10px)"})
+        }
+    }
+
+    const testimonial_tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: '.testimonial__grid',
+            start: 'top 90%',
+            end: 'top top',
+            scrub: 1,
+        }
+    })
+    testimonial_tl.from(".testimonial__title", {x: -500, opacity: 0, filter: "blur(10px)"})
+
+    const cardsTestimonial = document.querySelectorAll('.testimonial__card');
+
+    for(let i = 0; i < cardsTestimonial.length; i++){
+        const testimonial = cardsTestimonial[i];
+        if(i % 2){
+            testimonial_tl.from(testimonial, {x: -500, opacity: 0, filter: "blur(10px)"})
+        }else{
+            testimonial_tl.from(testimonial, {x: 500, opacity: 0, filter: "blur(10px)"})
+        }
+    }
+});
